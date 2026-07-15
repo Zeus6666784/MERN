4e982ee59d1365ce136a3752ec72ff89abc84c6f
LAVKUSH VISHWAKARMA <lavkvishmvlu@gmail.com>
2026-07-15 22:55:31 +0530

    Remove unused JavaScript files and add practical documents for MERN course

--- P2.js
+++
@@ -1,9 +0,0 @@
-function calculateSalary(salary, name) {
-    let bonus = 5000;
-    let totalSalary = salary + bonus;
-    console.log("Employee Name: " + name);
-    console.log("Base Salary: " + salary);
-    console.log("Bonus: " + bonus);
-    console.log("Total Salary: " + totalSalary);
-}
-calculateSalary(30000, "John Doe");
